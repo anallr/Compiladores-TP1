b2b06a112621a2f325a1d15edb6008a0b584bf48
Ana Luisa Rodrigues <anallr@dcc.ufmg.br>
2019-09-10 11:49:25 -0300

    analisador lexico ok

--- AnalisadorLexico/src/tp1compiladores/TesteAnalisadorLexico.java
+++ AnalisadorLexico/src/tp1compiladores/TesteAnalisadorLexico.java
@@ -1,4 +1,4 @@
-package br.edu.unoesc;
+package tp1compiladores;
 
 import java.io.IOException;
 import java.io.StringReader;
@@ -6,7 +6,7 @@ import java.io.StringReader;
 public class TesteAnalisadorLexico {
 	public static void main (String [] args) throws IOException{
 		
-		String expr = "integer a = 3 + 2;";
+		String expr = "integer a = 3 + 2";
 		AnalisadorLexico lexico = new AnalisadorLexico (new StringReader(expr));
 		lexico.yylex();
 	}
